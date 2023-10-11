49d3ef7fd66921ba7cb9f9c84c0201335e6f9fc7
AlejandroErnestoSanchezHernandez <124760119+AlejandroErnestoSanchezHernandez@users.noreply.github.com>
2023-10-10 19:03:27 -0600

    Add files via upload

---
+++ tarea4/form.php
@@ -0,0 +1,12 @@
+<?php
+   $nombre=$_POST["txtnombre"];
+   $edad=$_POST["txtedad"];
+   $correo=$_POST["txtcorreo"];
+   $pass=$POST["txtpasswd"];
+
+   echo "<p>Mi nombre es:" $nombre  "</p>";
+   echo "<p>Mi edad es:" .$edad . "</p>";
+   echo "<p>Mi correo es:" .$correo . "</p>";
+   echo "<p>Mi mi password es:" .$pass . "</p>";
+   echo "<a href='index.php'>Al inicio</a>";
+?>
---
+++ tarea4/index.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+     <form action="form.php" method="post">
+
+      Nombre:<input name="txtnombre">
+      Edad:<input correo="txtedad">
+      E-mail:<input edad="txtcorreo">
+      Password:<input passwd="txtpasswd">
+
+
+      <button>Enviar</button>
+
+    </form>
+
+    </body>
+</html>
