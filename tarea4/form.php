899f24498ce67133a2c594306860441581eb2ba9
AlejandroErnestoSanchezHernandez <124760119+AlejandroErnestoSanchezHernandez@users.noreply.github.com>
2023-10-10 21:18:28 -0600

    Add files via upload

--- tarea4/form.php
+++ tarea4/form.php
@@ -1,12 +1,15 @@
 <?php
-   $nombre=$_POST["txtnombre"];
-   $edad=$_POST["txtedad"];
-   $correo=$_POST["txtcorreo"];
-   $pass=$POST["txtpasswd"];
+if ($_SERVER["REQUEST_METHOD"] ){
+   $nombre = $_POST["txtnombre"];
+   $edad = $_POST["txtedad"];
+   $correo = $_POST["txtcorreo"];
+   $pass = $_POST["txtpasswd"];
 
-   echo "<p>Mi nombre es:" $nombre  "</p>";
-   echo "<p>Mi edad es:" .$edad . "</p>";
-   echo "<p>Mi correo es:" .$correo . "</p>";
-   echo "<p>Mi mi password es:" .$pass . "</p>";
+   echo "<p>Mi nombre es: " . $nombre .  "</p>";
+   echo "<p>Mi edad es: " . $edad . "</p>";
+   echo "<p>Mi correo es: " . $correo . "</p>";
+   echo "<p>Mi contraseña es: " . $pass . "</p>";
    echo "<a href='index.php'>Al inicio</a>";
+   exit();
+}
 ?>
--- tarea4/index.php
+++ tarea4/index.php
@@ -2,10 +2,10 @@
     <body>
      <form action="form.php" method="post">
 
-      Nombre:<input name="txtnombre">
-      Edad:<input correo="txtedad">
-      E-mail:<input edad="txtcorreo">
-      Password:<input passwd="txtpasswd">
+      Nombre: <input name="txtnombre">
+      Edad: <input correo="txtedad">
+      E-mail: <input edad="txtcorreo">
+      Password: <input passwd="txtpasswd">
 
 
       <button>Enviar</button>
